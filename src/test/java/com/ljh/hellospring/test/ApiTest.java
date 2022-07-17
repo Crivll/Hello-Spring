30a19e328e8b609c5192d2b7b8d2cd7d513bf18d
Crivll <1225441967@qq.com>
2022-07-17 23:24:50 +0800

    6. 实现资源加载器解析文件并自动注册：单元测试，添加xml和properties文件测试

--- src/test/java/com/ljh/hellospring/test/ApiTest.java
+++ src/test/java/com/ljh/hellospring/test/ApiTest.java
@@ -1,16 +1,23 @@
 package com.ljh.hellospring.test;
 
+import cn.hutool.core.io.IoUtil;
 import com.ljh.hellospring.beans.PropertyValue;
 import com.ljh.hellospring.beans.PropertyValues;
 import com.ljh.hellospring.beans.factory.config.BeanDefinition;
 import com.ljh.hellospring.beans.factory.config.BeanReference;
 import com.ljh.hellospring.beans.factory.support.DefaultListableBeanFactory;
+import com.ljh.hellospring.beans.factory.xml.XmlBeanDefinitionReader;
+import com.ljh.hellospring.core.io.DefaultResourceLoader;
+import com.ljh.hellospring.core.io.Resource;
 import com.ljh.hellospring.test.bean.UserDao;
 import com.ljh.hellospring.test.bean.UserService;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.NoOp;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -101,5 +108,51 @@ public class ApiTest {
         // 5. UserService 获取bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
+    }
+
+    private DefaultResourceLoader resourceLoader;
+
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        Resource resource = resourceLoader.getResource("https://github.com/Crivll/Hello-Spring/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_xml() {
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        // 2. 读取配置文件&注册Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
+
+        // 3. 获取Bean对象调用方法
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
     }
 }
